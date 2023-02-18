29c20cb2027fbc1dde2d68c2204d42eba9d0c234
Carina Payleman <rociocarina2@gmail.com>
2023-02-18 18:21:37 -0300

    Termina función automática para añadir product cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const cartMenu = document.querySelector('.navbar__shopping-cart');
 const aside = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container');
 
 const toggleElement = (element) => {
     element.classList.toggle('inactive');
@@ -11,6 +12,89 @@ const toggleElement = (element) => {
 const hideElement = (element) => {
     element.classList.add('inactive')
 }
+
+let productList = []
+productList.push({
+    name: 'Candelabro',
+    price: 120,
+    img: 'https://images.pexels.com/photos/937526/pexels-photo-937526.jpeg?auto=compress&cs=tinysrgb&w=600'
+})
+productList.push({
+    name: 'Sillón',
+    price: 120,
+    img: 'https://images.pexels.com/photos/3757055/pexels-photo-3757055.jpeg?auto=compress&cs=tinysrgb&w=600'
+})
+productList.push({
+    name: 'Cámara vintage',
+    price: 94,
+    img: 'https://images.pexels.com/photos/1327503/pexels-photo-1327503.jpeg?auto=compress&cs=tinysrgb&w=1600'
+})
+productList.push({
+    name: 'Bicicleta',
+    price: 60,
+    img: 'https://images.pexels.com/photos/1149601/pexels-photo-1149601.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+})
+productList.push({
+    name: 'Globo Terráqueo',
+    price: 35,
+    img: 'https://images.pexels.com/photos/7634594/pexels-photo-7634594.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+})
+
+function createProductCard () {
+    /*
+    <div class="product-card">
+        <img src="https://images.pexels.com/photos/937526/pexels-photo-937526.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" class="product-card__img">
+        <div class="product-card__info">
+            <div>
+                <p>$120,00</p>
+                <p>Candelabro</p>
+            </div>
+            <figure>
+                <img src="assets/icons/bt_add_to_cart.svg" alt="Añadir al carrito">
+            </figure>
+        </div>
+    </div>
+    */
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const productImg = document.createElement('img');
+    productImg.classList.add('product-card__img');
+    productImg.setAttribute('src', product.img);
+
+    const productCardInfo = document.createElement('div');
+    productCardInfo.classList.add('product-card__info');
+
+    const productCardInfoDiv = document.createElement('div');
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+    productCardInfoDiv.appendChild(productPrice); 
+    productCardInfoDiv.appendChild(productName); 
+
+    const productCardFigure = document.createElement('figure');
+    const imgCart = document.createElement('img');
+    imgCart.setAttribute('src', 'assets/icons/bt_add_to_cart.svg');
+    productCardFigure.appendChild(imgCart); 
+
+    productCardInfo.appendChild(productCardInfoDiv); 
+    productCardInfo.appendChild(productCardFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productCardInfo);
+
+
+    cardsContainer.appendChild(productCard);
+}
+
+for (product of productList) {
+    createProductCard();
+}
+
+
+
+
 
 menuEmail.addEventListener('click', () => {
     toggleElement(desktopMenu);
