a8f977bcd72b6b5cd5dbc4e8ea5b8576d34b6bce
Carina Payleman <rociocarina2@gmail.com>
2023-02-18 15:18:37 -0300

    Cambia nombre función toggleElement, añade toggle de carrito

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,19 @@ const menuEmail = document.querySelector('.navbar__email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const cartMenu = document.querySelector('.navbar__shopping-cart');
+const aside = document.querySelector('.product-detail')
 
-const toggleMenu = (element) => {
+const toggleElement = (element) => {
     element.classList.toggle('inactive');
 }
 
 menuEmail.addEventListener('click', () => {
-    toggleMenu(desktopMenu);
+    toggleElement(desktopMenu);
 });
 burgerMenu.addEventListener('click', () => {
-    toggleMenu(mobileMenu);
+    toggleElement(mobileMenu);
 });
+cartMenu.addEventListener('click', () => {
+    toggleElement(aside);    
+})
