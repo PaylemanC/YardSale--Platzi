13f707e96e111094b49e6fe658deefc356106e8a
Carina Payleman <rociocarina2@gmail.com>
2023-06-05 22:36:27 -0300

    Renderiza datos de producto con TypeScript

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
+  constructor() { }
 
+  public product: Product = {
+    id: 1, 
+    title: 'Candelabro',
+    price: 120,
+    img: 'https://images.pexels.com/photos/937526/pexels-photo-937526.jpeg?auto=compress&amp;cs=tinysrgb&amp;w=600',
+    description: ''
+  }
 }
