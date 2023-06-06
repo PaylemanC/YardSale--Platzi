35fc7b6a0454e241352d56125009ca1700cc42ec
Carina Payleman <rociocarina2@gmail.com>
2023-06-06 02:50:31 -0300

    Crea primer servicio: shopping cart

---
+++ src/app/services/shopping-cart.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product.model';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingCartService {
+  constructor() { }
+
+  private myShoppingCart: Product[] = [];
+
+  private myCart = new BehaviorSubject<Product[]>([]);
+
+  myCart$ = this.myCart.asObservable();
+
+  addProduct(product: Product) {
+    this.myShoppingCart.push(product);
+    this.myCart.next(this.myShoppingCart);
+  }
+
+  getTotal() {
+    return this.myShoppingCart.reduce((sum, item) => sum + item.price, 0);
+  }
+
+  getShoppingCart() {
+    return this.myShoppingCart;
+  }
+}
