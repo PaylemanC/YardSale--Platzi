a246716640e6b1da0d9295f5a089507dcfe9fbd4
Carina Payleman <rociocarina2@gmail.com>
2023-08-17 19:00:46 -0300

    Aplica routing hacia home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  template: `<app-products-list></app-products-list>`,
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
