0673bbc5b76ffb35b40487fd27fc7903c0206a75
Carina Payleman <rociocarina2@gmail.com>
2023-08-17 18:59:26 -0300

    Añade componentes pages al módulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     ProductComponent,
-    ProductsListComponent
+    ProductsListComponent,
+    HomeComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
